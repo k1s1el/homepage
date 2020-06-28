65caea0f15cd23dc7f2d0e289465d62c86b2aa7b
Marcin Kisielewski <mkisielewski25@gmail.com>
2020-06-28 23:48:07 +0200

    refactored code for button2

--- js/script.js
+++ js/script.js
@@ -1,44 +1,52 @@
-{
-    const welcome = () => {
-        console.log("Hello World!!")
-    };
-    welcome();
-}
+const welcome = () => {
+    console.log("Hello World!!")
+};
 
+welcome();
 
 const onChangeNameHeaderClick = () => {
 
     name1.innerHTML = "Marcin Kisielewski";
 }
+
+const onToggleBackgroundClick = () => {
+
+    const backgroundCol = document.querySelector(".body");
+    backgroundCol.classList.toggle("body2");
+
+    if (backgroundCol.classList.contains("body2")) {
+        changeBackgroundButton.innerText = "Zmień tło na jaśniejsze";
+    }
+    else {
+        changeBackgroundButton.innerText = "Zmień tło na ciemniejsze";
+    };
+
+}
+
+const changeBackgroundButton = document.querySelector(".button2");
+changeBackgroundButton.addEventListener("click", onToggleBackgroundClick);
+
 const init = () => {
     const button1 = document.querySelector(".button1");
     button1.addEventListener("click", onChangeNameHeaderClick);
+
 }
 
 init();
 
 
-let button2 = document.querySelector(".button2");
+
 let button3 = document.querySelector(".button3");
 let button4 = document.querySelector(".button4");
 let name1 = document.querySelector(".name1");
-let backgroundCol = document.querySelector(".body");
+
 let face1 = document.querySelector(".face1")
 let work1 = document.querySelector(".work1")
 
 
 
 
-button2.addEventListener("click", () => {
-    backgroundCol.classList.toggle("body2");
 
-    if (backgroundCol.classList.contains("body2")) {
-        button2.innerText = "Zmień tło na jaśniejsze";
-    }
-    else {
-        button2.innerText = "Zmień tło na ciemniejsze";
-    };
-});
 
 button3.addEventListener("click", () => {
     work1.classList.toggle("work2")
